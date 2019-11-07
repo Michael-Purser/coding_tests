bbc6c0d1e3afc32db7ec330cf9b63ea17cdb861c
Michael Purser <michael.purser@outlook.be>
2019-11-08 00:04:01 +0100

    [unordere_maps] Further tests on threads in maps

--- unordered_maps/src/main.cpp
+++ unordered_maps/src/main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <mutex>
 #include <string>
 #include <thread>
+#include <unistd.h>
 #include <unordered_map>
+#include <vector>
 
-void func()
+struct threadPIDVector
 {
-  std::cout << "Hello from thread" << std::endl;
+  void initialize(int size);
+  int getPID(int index);
+  void setPID(int index, int PID);
+
+  std::vector<int> PID_vector;
+  std::mutex mutex;
+};
+
+void threadPIDVector::initialize(int size)
+{
+  std::lock_guard<std::mutex> guard{mutex};
+  return PID_vector.assign(size, 0);
+}
+
+int threadPIDVector::getPID(int index)
+{
+  std::lock_guard<std::mutex> guard{mutex};
+  return PID_vector.at(index);
+}
+
+void threadPIDVector::setPID(int index, int PID)
+{
+  std::lock_guard<std::mutex> guard{mutex};
+  PID_vector.at(index) = PID;
+}
+
+threadPIDVector thread_PID_vector;
+
+void func(int index)
+{
+  // std::cout << "Hello from thread" << std::endl;
+  thread_PID_vector.setPID(index, static_cast<int>(getpid()));
+  std::cout << "Hello from thread " << index << ", my PID is: " << thread_PID_vector.getPID(index) << std::endl;
+  system("/home/michael/Documents/CODING/unordered_maps/test.sh > /dev/null &");
+  std::cout << "Hello from end of thread function" << std::endl;
 }
 
 int
@@ -44,14 +83,19 @@ main(int argc, char** argv)
 
   std::unordered_map<int, std::thread> map2;
 
+  thread_PID_vector.initialize(4);
+
   // Insert some values into the map:
-  map2.insert({1, std::thread(func)});
-  map2.insert({3, std::thread(func)});
-  map2.insert({300, std::thread(func)});
-  map2.insert({56, std::thread(func)});
+  map2.insert({1, std::thread(func, 0)});
+  map2.insert({3, std::thread(func, 1)});
+  map2.insert({300, std::thread(func, 2)});
+  map2.insert({56, std::thread(func, 3)});
+
+  std::this_thread::sleep_for(std::chrono::seconds(2));
 
   for(auto& pair : map2)
   {
+    // pair.second.~thread();
     pair.second.join();
     std::cout << "Joining thread at key: " << pair.first << std::endl;
   }
